57791ef5a9c690c537de34670cb6d133d040f839
SaulParzy19 <italo.vecchi@polocodigoro.edu.it>
2023-03-08 12:29:06 +0100

    Aggiunto import Vecchi

--- saluto.py
+++ saluto.py
@@ -1,5 +1,8 @@
+import italo_vecchi_Gruppo19
+
 def saluto_Parisi_Gruppo19():
     saluto = "Buongiorno, questo è il contributo del Gruppo19"
     print(saluto)
 
 saluto_Parisi_Gruppo19()
+italo_vecchi_Gruppo19.saluto_Vecchi_Gruppo19()
